809a8874a379caa5df80224d6eb36e7862260cb1
faaraad <farhad.barahimi@gmail.com>
2024-05-31 21:51:46 +0330

    add README.md

--- goc.py
+++ goc.py
@@ -1,8 +1,8 @@
 from crawl import crawl_website
-
+import os
 
 
 base_url = "https://immi.homeaffairs.gov.au/visas/working-in-australia/skill-occupation-list"
-driver = "/usr/lib/chromium-browser/chromedriver"
+driver = os.environ.get("chromedriver_addr", "/usr/lib/chromium-browser/chromedriver")
 
 crawl_website(base_url, driver)
